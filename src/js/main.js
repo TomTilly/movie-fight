01c07861ab7f79befc5884c08baf6cfcaaa298ea
Tom Tillistrand <tomtillistrand@gmail.com>
2021-05-23 15:49:47 -0400

    Remove dropdown when no search results found

--- src/js/main.js
+++ src/js/main.js
@@ -13,29 +13,36 @@ const input = document.querySelector('[name="movie-1"]');
 
 const onInput = async (event) => {
   const { target } = event;
-  if (target.value !== '') {
-    const movies = await fetchData(omdbBaseUrl, {
-      apikey: process.env.API_KEY,
-      s: target.value,
-    });
+  const dropdown = target.parentElement.querySelector('.dropdown');
 
-    const dropdown = target.parentElement.querySelector('.dropdown');
+  if (!target.value) {
+    dropdown.classList.remove('is-active');
+    return;
+  }
+
+  const movies = await fetchData(omdbBaseUrl, {
+    apikey: process.env.API_KEY,
+    s: target.value,
+  });
+
+  // Clear old suggestions
+  while (dropdown.firstChild) {
+    dropdown.removeChild(dropdown.firstChild);
+  }
+
+  if (!movies.length) {
+    dropdown.classList.remove('is-active');
+  } else {
     dropdown.classList.add('is-active');
+  }
 
-    // Clear old suggestions
-    while (dropdown.firstChild) {
-      dropdown.removeChild(dropdown.firstChild);
-    }
-
-    for (let i = 0; i < 5; i += 1) {
-      if (movies[i]) {
-        console.log(movies[i]);
-        const movie = movies[i];
-        const suggestion = document.createElement('li');
-        suggestion.classList.add('suggestion');
-        suggestion.innerHTML = `<img src="${movie.Poster}" alt="${movie.Title}" width="68" height="100"> ${movie.Title}`;
-        dropdown.appendChild(suggestion);
-      }
+  for (let i = 0; i < 5; i += 1) {
+    if (movies[i]) {
+      const movie = movies[i];
+      const suggestion = document.createElement('li');
+      suggestion.classList.add('suggestion');
+      suggestion.innerHTML = `<img src="${movie.Poster}" alt="${movie.Title}"> ${movie.Title}`;
+      dropdown.appendChild(suggestion);
     }
   }
 };
